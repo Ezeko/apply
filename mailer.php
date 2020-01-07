c135e385004c66cdc96bb593172d938dcff9b9fb
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-01-07 15:09:30 +0100

    email sender

--- mailer.php
+++ mailer.php
@@ -1,9 +1,6 @@
 <?php
   $email= $_REQUEST['email'];
-  $firstname = $_REQUEST['firstname'];
-  $lastname = $_REQUEST['lastname'];
-  $ticketID = $_REQUEST['ticketID'];
-  $team = "ADMIN";
+
   $to = $email;
   $headers  = "MIME-Version: 1.0" . "\r\n";
   $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
@@ -11,17 +8,30 @@
   $subject = $_REQUEST['subject'];
   $message = '<html>
                   <body style="color:grey">
-                      <p style="color: green">Hi '.$firstname.' '.$lastname.'</p>
+                      <p style="color: green">Dear Hiring Manager,
+
+                      APPLICATION FOR THE POST OF A WEBSITE DEVELOPER
+                      
+                      </p>
                       <p>
                           We recieved below details from you. Please use given Request/Ticket ID for future follow up:
                       </p>
                       <p>
-                          Your Request/Ticket ID: <b>'.$ticketID.'</b>
+                      I write in response to your post about your search to fill the vacant position in your organization.
+                      
+                      I am a seasoned fullstack website developer with core competencies in html5, css3, bootsrap4, JavaScript, php mysql, mongodb, react, expressJs and NodeJs.
+                      
+                      I have full knowledge of version control system (git). I also understand fully well unit and integrated testing and also have an in-depth knowledge of agile frameworks.
+                      
+                      Kindly find below the attached copy of my resume. Looking to hearing from you soon. Thanks for your time and consideration.
+                      
+                    
                       </p>
                       <p>
-                      Thanks,<br>
-                      '.$team.' Team.
+                      Yours Sincerely,<br>
+                      Ezekiel ADEJOBI.
                       </p>
+                      <a href=''>Click here to view resume</a>
                   </body>
               </html>';
   $success = mail( $to, $subject, $message, $headers ); 
